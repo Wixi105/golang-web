7ad927dbdb77e726aea13498cf94353e85c00766
wixi105 <ericsampson105@gmail.com>
2022-05-09 11:10:51 +0000

    added base and footer partials
    Signed-off-by: wixi105 <ericsampson105@gmail.com>

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
+
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Snippetbox"))
+
+	// Use the template.ParseFiles to load the template into a template set
+	// if there is an error, we log the error to the console and return
+	// an http Internal Server Error to the response writer.
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	// you can pass the files as a variadic parameter to the template.ParseFiles function.
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	// w.Write([]byte("Hello from Snippetbox"))
 }
+
+
+
+// ================================= showSnippetHandler Start ==========================================
 
 func showSnippetHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
@@ -23,6 +56,11 @@ func showSnippetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 
 }
+
+// ================================= showSnippetHandler End ==========================================
+
+
+
 
 func createSnippetHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
